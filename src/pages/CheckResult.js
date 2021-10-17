803372ad56d90bdb23eb1075bc0ce4d7b2996463
harsh raj gupta <60179650+ha280@users.noreply.github.com>
2021-10-18 00:40:12 +0530

    .

--- src/pages/CheckResult.js
+++ src/pages/CheckResult.js
@@ -3,22 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './check.css'
 import { listenerCount } from 'process';
 import { useState} from 'react';
-
+import list from '../8wn96-5xm49.json'
 
 const CheckResult = () => {
   const [selected, setSelected] = useState('');
-  var list=[
-    {
-      "walletAdderss" : "hfdnjnvjfdnjnvejdmnjmnm",
-    },
-    {
-      "walletAdderss" : "49808eodufrdjm",
-    }
-  ];
+  // var list=[
+  //   {
+  //     "walletAdderss" : "hfdnjnvjfdnjnvejdmnjmnm",
+  //   },
+  //   {
+  //     "walletAdderss" : "49808eodufrdjm",
+  //   }
+  // ];
   const [info, setInfo] = useState('-');
   const handleSubmit = async () => {
     for(var i=0;i<list.length;i++){
-      if(list[i].walletAdderss==selected){
+      if(list[i].WalletAddress==selected){
         setInfo("Congratulations, You’re in!");
         break;
       }
@@ -34,7 +34,7 @@ const CheckResult = () => {
         <h2 class="heading">#REF1ECT</h2>
         <h1 class="heading border-bottom">THE WHITELIST</h1>
         <p class = "py-5">Fill in your wallet address below to check if you made it.</p>
-        <p secondary class="text-center tect">{info}</p>
+        <p secondary class="text-center ">{info}</p>
         <input type="text" class="w-100  text-light bg-dark" placeholder="Paste the wallet address" value={selected}
 							onChange={(e) => {
 								setSelected(e.target.value);
