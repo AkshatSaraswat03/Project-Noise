4ef7c63b9127eb7ac2c4b237f539845f1cc8cc7e
harsh raj gupta <harshraj123gupta@gmail.com>
2021-11-23 20:04:15 +0000

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import GalleryPage from './pages/GalleryPage';
 import ArtPage from './pages/ArtPage';
 import MintPage from './pages/MintPage';
 import CheckResult from './pages/CheckResult';
+import newHome from './pages/newHome';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route exact path='/art' component={ArtPage} />
         <Route exact path='/mint' component={MintPage} />
         <Route exact path='/whitelist-checker' component={CheckResult} />
+        <Route exact path='/newhome' component={newHome} />
       </Switch>
     </Router>
 
